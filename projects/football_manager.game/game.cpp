3f9f16d9ec415101fe0eb35f16326a71bf7b565b
Patrick Craveiro Gannon <patrickg258@gmail.com>
2018-01-30 11:34:53 +0000

    Added League and Game cpp/hpp

---
+++ projects/football_manager.game/game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+void game(){
+
+    class game {
+
+        std::string homeTeamName;
+        std::string awayTeamName;
+        int time;
+        int homeScore;
+        int awayScore;
+    };
+}
---
+++ projects/football_manager.game/league.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+void league() {
+
+    class league {
+
+        std::string teamName;
+        int wins;
+        int loses;
+        int points;
+        int gamesPlayed;
+    };
+
+    std::cout << "working" << std::endl;
+
+}
