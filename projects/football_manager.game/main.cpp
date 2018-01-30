0d66499dbbe889803bcd943f4e4e5ec128179247
Niam Islam <niamislam99@gmail.com>
2018-01-30 11:24:15 +0000

    create menu class, and function

--- projects/football_manager.game/main.cpp
+++ projects/football_manager.game/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "menu.hpp"
 
 int main()
 {
     std::cout << "Hello" << std::endl;
+    callMenu();
     return 0;
 }
---
+++ projects/football_manager.game/menu.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+class Menu {
+public:
+    std::string name;
+    int choice;
+    std::string prompt;
+};
+
+int callMenu()
+{
+    Menu chooseTeam;
+    chooseTeam.name = "Choose a team";
+    chooseTeam.choice = 1;
+    chooseTeam.prompt = "Here are the teams: ";
+
+    Menu info;
+    info.name = "Information";
+    info.choice = 2;
+    info.prompt = "Information: ";
+
+    Menu quit;
+    quit.name = "Quit";
+    quit.choice = 0;
+    quit.prompt = "Thanks for playing!";
+
+    std::cout << "Main Menu" << std::endl;
+    std::cout << "[" << chooseTeam.choice << "]" << " " << chooseTeam.name << std::endl;
+    std::cout << "[" << info.choice << "]" << " " << info.name << std::endl;
+    std::cout << "[" << quit.choice << "]" << " " << quit.name << std::endl;
+    std::cout << std::endl;
+
+    int userChoice;
+    bool choiceValid = false;
+
+    do{
+        std::cout << "Enter your choice: ";
+        std::cin >> userChoice;
+        if (userChoice == 1) {
+            std::cout << chooseTeam.prompt << std::endl;
+            // view teams function
+            choiceValid = true;
+        } else if (userChoice == 2) {
+            std::cout << info.prompt << std::endl;
+            choiceValid = true;
+        } else if (userChoice == 0) {
+            std::cout << quit.prompt << std::endl;
+            exit(0);
+        } else {
+            std::cout << "Please enter a valid menu choice, e.g 0, 1, 2" << std::endl;
+        }
+    } while (choiceValid == false);
+}
