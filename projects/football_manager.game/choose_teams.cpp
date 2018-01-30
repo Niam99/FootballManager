15b1694a76f937ac92c873efadbfe285213bb7d7
Patrick Craveiro Gannon <patrickg258@gmail.com>
2018-01-30 18:19:45 +0000

    Attempted to add teams using Vector

---
+++ projects/football_manager.game/choose_teams.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include "choose_teams.hpp"
+
+
+
+std::vector<std::string> teams_class::teams(team_name_zz,std:: end(team_name_zz));
+
+void choose_team(){
+
+    std::string team_name;
+
+    std::cout << team_name_zz[0] << std::endl;
+}
---
+++ projects/football_manager.game/choose_teams.hpp
@@ -0,0 +1,14 @@
+#ifndef CHOOSE_TEAMS_HPP
+#define CHOOSE_TEAMS_HPP
+
+
+#include <string>
+#include <vector>
+
+class teams_class {
+    static std::vector<std::string> teams; // declaration
+};
+
+const char *team_name_zz[] = {"one", "two", "three"};
+
+#endif
