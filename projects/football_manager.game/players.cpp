523141bfa21a9942638901be8f91d815e3b423be
Niam Islam <niamislam99@gmail.com>
2018-01-30 16:50:20 +0000

    make class for players

---
+++ projects/football_manager.game/players.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+void players() {
+
+    class Players {
+        std::string name;
+        std::string team;
+        int attack;
+        int defense;
+        std::string position;
+    };
+}
---
+++ projects/football_manager.game/players.hpp
@@ -0,0 +1,1 @@
+void players();c
