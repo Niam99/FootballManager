58b7ee5775e2e9579face7db4700d4d6a314b050
Patrick Craveiro Gannon <patrickg258@gmail.com>
2018-02-05 10:57:10 +0000

    Set Tasks, learning pointers

--- class_practice.Pat/main.cpp
+++ class_practice.Pat/main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 #include <string>
 #include "classpractice.hpp"
@@ -5,10 +6,15 @@
 int main()
 {
     std::cout << "I'll just put this here\n";
-    
     Employee pat {"Patrck", 1, 8.39};
     Employee john {"John", 1, 10.39};
     Employee steve {"Steve", 1, 12.39};
+
+
+    std::vecto<rEmployee> collection_employees;
+    collection_employees.push_back(pat);
+
+     push_back(pat);
 
     pat.print();
     john.print();
