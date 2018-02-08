dbb15e221a7947016532f0ad0fbd3e088f939879
Niam Islam <niamislam99@gmail.com>
2018-02-08 14:24:46 +0000

    add another employee to program

--- projects/niam_practice/pointer_employee2.Niam/main.cpp
+++ projects/niam_practice/pointer_employee2.Niam/main.cpp
@@ -4,6 +4,7 @@
 int main() {
     Employee Employee1(9.0, 11.2, 3.0,0.2);
     Employee Employee2(8.5, 6.0, 2.0,0.2);
+    Employee Employee3(7.5, 23.5, 7.0,0.2);
     Employee *ptrEmployee;
     ptrEmployee = &Employee1;
 
@@ -17,5 +18,13 @@ int main() {
     std::cout << "Employee 2 pay: £ " << ptrEmployee->Pay() << std::endl;
     p = ptrEmployee->Pay();
     std::cout << "Money after tax: £ " << ptrEmployee->Tax(p) << std::endl;
+    std::cout << "------------------------" << std::endl;
+
+    ptrEmployee = &Employee3;
+
+    std::cout << "Employee 3 pay: £ " << ptrEmployee->Pay() << std::endl;
+    p = ptrEmployee->Pay();
+    std::cout << "Money after tax: £ " << ptrEmployee->Tax(p) << std::endl;
+    std::cout << "------------------------" << std::endl;
     return 0;
 }
