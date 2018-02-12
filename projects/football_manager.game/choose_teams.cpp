502b8b0116ef55921010d3fce514c920a96416e9
Patrick Craveiro Gannon <patrickg258@gmail.com>
2018-02-12 11:49:59 +0000

    Added players to team class, cleaned team menu

--- projects/football_manager.game/choose_teams.cpp
+++ projects/football_manager.game/choose_teams.cpp
@@ -1,71 +1,87 @@
 #include <iostream>
 #include <string>
 #include<bits/stdc++.h>
-#include "menu.hpp"
+//#include "menu.hpp"
+#include "choose_teams.hpp"
 
-menu choose_teams(){
+void choose::choose_teams(){
 
+    choose team_player;
+    
+    //void choose_teams();
+    
     bool teamSelected = false;
 
     while (teamSelected == false) {
-        //std::vector <std::string> team;
+        std::vector <std::string> team;
         int selection;
         std::cout << "\nTeams:" << std::endl << std::endl;
-
-        //team_menu team;
-        //.setup("BLANK");
-        //.pushback(team);
         
-        //team.push_back ("Manchester-United");
-        //team.push_back("Chelsea");
-        //team.push_back("Arsenal");
-        //team.push_back("Manchester-City");
-        //team.push_back("Liverpool");
+        team.push_back("BLANK");
+        
+        team.push_back ("Manchester-United");
+        team.push_back("Chelsea");
+        team.push_back("Arsenal");
+        team.push_back("Manchester-City");
+        team.push_back("Liverpool");
 
-        // for (int i=1; i<team.size(); i++) {
-        // std::cout<< "[" << i << "] " << team[i] << "\n";
+        for (int i=1; i<team.size(); i++) {
+        std::cout<< "[" << i << "] " << team[i] << "\n";
 
-        // }
+        }
         
         std::cout << "Choose a team: ";
         std::cin >> selection;
 
         // create an instance of the menu class.
-        menu main_menu = choose_teams();
+        //menu main_menu = choose_teams();
         //showplayers()
 
         // "run" it
-        int result = main_menu.run();
-        
+        //int result = main_menu.run();
+
         if (selection == 1){
-            // std::cout << "\nYou have selected:\n\n" << team[1] << std::endl
-            //<< std::endl;
+            std::cout << "\nYou have selected:\n\n" << team[1] << std::endl
+            << std::endl;
 
-            std::cout << "Squad:\n"
-                      << "GK - David de Gea\n"
-                      << "LB - Daley Blind\n"
-                      << "RB - Antonio Valencia\n"
-                      << "CB - Victor Lindelöf\n"
-                      << "CB - Phil Jones\n"
-                      << "LW - Alexis Sánchez\n"
-                      << "RW - Anthony Martial\n"
-                      << "CM - Marouane Fellaini\n"
-                      << "CM - Paul Pogba\n"
-                      << "CF - Romelu Lukaku\n"
-                      << "CF - Zlatan Ibrahimovic\n" << std::endl;
+            team_player.GK = "David de Gea";
+            team_player.LB = "Daley Blind";
+            team_player.RB = "Antonio Valencia";
+            team_player.CB = "Victor Lindelöf";
+            team_player.CB2 = "Phil Jones";
+            team_player.LW = "Alexis Sánchez";
+            team_player.RW = "Anthony Martial";
+            team_player.CM = "Marouane Fellaini";
+            team_player.CM2 = "Paul Pogba";
+            team_player.CF = "Romelu Lukaku";
+            team_player.CF2 = "Zlatan Ibrahimovic";
+        }
 
-            //std::cout << "Confirm " << team[1] << "?" << std::endl;
-            std::cout << "[0] Yes\n[1] No" << std::endl;
+        std::cout << "Squad:\n"
+                  << "GK - " << team_player.GK << std::endl
+                  << "LB - " << team_player.LB << std::endl
+                  << "RB - " << team_player.RB << std::endl
+                  << "CB - " << team_player.CB << std::endl
+                  << "CB - " <<  team_player.CB2 << std::endl
+                  << "LW - " << team_player.LW << std::endl
+                  << "RW - " << team_player.RW << std::endl
+                  << "CM - " << team_player.CM << std::endl
+                  << "CM - " << team_player.CM2 << std::endl
+                  << "CF - " << team_player.CF << std::endl
+                  << "CF - " << team_player.CF2 << std::endl;
+
+        std::cout << "\nConfirm Selected team?" << std::endl;
+
+        std::cout << "[0] Yes\n[1] No" << std::endl;
             std::cin >> selection;
 
             if (selection == 0) {
                 teamSelected = true;
-                return main_menu;
+                //return;
             }
 
             if (selection == 1) {
                 continue;
             }
-        }
     }
 }
--- projects/football_manager.game/choose_teams.hpp
+++ projects/football_manager.game/choose_teams.hpp
@@ -1,1 +1,29 @@
-void choose_teams();
+#ifndef CHOOSE_TEAMS_HPP
+#define CHOOSE_TEAMS_HPP
+
+#include <string>
+#include <vector>
+
+class choose{
+
+public:
+    std::string GK;
+    std::string LB;
+    std::string RB;
+    std::string CB;
+    std::string CB2;
+    std::string LW;
+    std::string RW;
+    std::string CM;
+    std::string CM2;
+    std::string CF;
+    std::string CF2;
+
+    void choose_teams();
+};
+
+#endif
+
+
+//void choose_teams();
+ void choose_teams();
