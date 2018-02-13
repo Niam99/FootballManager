c85dfcc7c06c189013010b91a1f381de14e2cdd8
Niam Islam <niamislam99@gmail.com>
2018-02-13 11:05:14 +0000

    start game class

--- projects/football_manager.game/game.cpp
+++ projects/football_manager.game/game.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
+#include "game.hpp"
 
-void game(){
+void game::setup(std::string team1, std::string team2, int t1goals, int t2goals) {
 
-    class game {
+    team1_ = team1;
+    team2_ = team2;
+    t1goals_ = t1goals;
+    t2goals_ = t2goals;
+}
 
-        std::string homeTeamName;
-        std::string awayTeamName;
-        int time;
-        int homeScore;
-        int awayScore;
-    };
+void game::display() {
+    std::cout << "Team 1: " << team1_ << std::endl;
+    std::cout << "Team 2: " << team2_ << std::endl;
+    std::cout << team1_ << " scored " << t1goals_ << " goals." << std::endl;
+    std::cout << team2_ << " scored " << t2goals_ << " goals." << std::endl;
 }
--- projects/football_manager.game/game.hpp
+++ projects/football_manager.game/game.hpp
@@ -1,1 +1,18 @@
-void game();
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <iostream>
+#include <string>
+
+class game {
+public:
+    void setup(std::string team1, std::string team2, int t1goals, int t2goals);
+    void display();
+
+    std::string team1_;
+    std::string team2_;
+    int t1goals_;
+    int t2goals_;
+};
+
+#endif
