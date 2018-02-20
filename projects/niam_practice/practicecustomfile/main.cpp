f6e942039afe4ee60939b51cea928b68031fcdb3
Niam Islam <niamislam99@gmail.com>
2018-02-20 12:14:45 +0000

    add multiple variables to readcustomfile program

--- projects/niam_practice/practicecustomfile/main.cpp
+++ projects/niam_practice/practicecustomfile/main.cpp
@@ -7,9 +7,15 @@ int main()
    std::ifstream theFile("players.txt");
    int id;
    std::string name;
-   int rating;
-   
-   while (theFile >> id >> name >> rating) {
-       std::cout << id << ". " << name << ": " << rating << std::endl;
+   int attack;
+   int defense;
+   std::string pos;
+   std::cout << "Player data:" << std::endl; 
+   while (theFile >> id >> name >> attack >> defense >> pos) {
+       std::cout << id << std::endl;
+       std::cout << "Name: " << name << std::endl;
+       std::cout << "Attack: " << attack << std::endl;
+       std::cout << "Defense: " << defense << std::endl;
+       std::cout << "Position: " << pos << std::endl;
    }
 }
